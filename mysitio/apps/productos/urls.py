770e69d23bd367f1cdd21063dc43d46726287972
alejandrope07 <alexwiper071@outlook.com>
2019-05-26 23:41:35 -0600

    actu

--- mysitio/apps/productos/urls.py
+++ mysitio/apps/productos/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
  
-from apps.productos.views import ProductoCrear, ProductoList, ProductoDelete
+from apps.productos.views import ProductoCrear, ProductoList, ProductoDelete, ProductoUpdate
 
 
 urlpatterns = [
@@ -8,6 +8,6 @@ urlpatterns = [
     path('nuevo', ProductoCrear.as_view(), name='producto_nuevo'),
     path('lista', ProductoList.as_view(), name='productos_lista'),
     path('eliminar/<int:pk>/', ProductoDelete.as_view(), name='productos_eliminar'),
-
+    path('editar/<int:pk>/', ProductoUpdate.as_view(), name='productos_editar'),
 
    ]
--- mysitio/apps/productos/views.py
+++ mysitio/apps/productos/views.py
@@ -25,3 +25,10 @@ class ProductoDelete(DeleteView):
 	model = Productos
 	template_name = 'sistema/productos_delete.html'
 	success_url = reverse_lazy('productos_lista')
+
+class ProductoUpdate(UpdateView):
+	
+	model = Productos
+	form_class = ProductoForm
+	template_name = 'sistema/producto_editar.html'
+	success_url = reverse_lazy('productos_lista')
